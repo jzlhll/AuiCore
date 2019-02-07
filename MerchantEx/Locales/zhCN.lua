4cce970d9861b9fcf3f3d5e8a431d224deabde30
allan.jiang.home <jzl.hll@163.com>
2019-02-07 22:33:20 +0800

    remove 爱不已

--- MerchantEx/Locales/zhCN.lua
+++ MerchantEx/Locales/zhCN.lua
@@ -8,7 +8,7 @@
 
 if GetLocale() == "zhCN" then
 	MERCHANTEX_LOCALE = {
-		["title"] = "爱不易",
+		["title"] = "MerchantEx",
 		["auto options"] = "自动选项",
 		["repair"] = "修理装备",
 		["guild repair"] = "公会银行支付",
